601a2200b815cf031370d2e2c02c2030198fd68b
resat <ozkurtresat@gmail.com>
2021-12-14 21:49:47 +0100

    BilletDevice added

---
+++ src/main/java/BilletDevice.java
@@ -0,0 +1,34 @@
+public class BilletDevice {
+    private int id;
+
+    public BilletDevice(int id) {
+        this.id = id;
+    }
+    public BilletDevice() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void scanDevices(){
+        System.out.println("devices area scanning");
+    }
+
+    public boolean detectDoorsBlocked(){
+        return true;
+    }
+
+    public BilletDevice getDevicesThatLeft(){
+        return new BilletDevice();
+    }
+
+    public int calculateStopsTravelled(){
+        return 10;
+    }
+
+}
